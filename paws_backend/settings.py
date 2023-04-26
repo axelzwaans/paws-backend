374ca78a70b9130f91dec365f9126d02959bc2d2
Axel Zwaans <axelzwaans@hotmail.com>
2023-04-26 07:37:17 +0000

    Add logout route and URL

--- paws_backend/settings.py
+++ paws_backend/settings.py
@@ -113,8 +113,9 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN_DEV')
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
